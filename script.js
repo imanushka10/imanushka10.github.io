6c647b9dbe96858792dbe6b4f5ed6c731a66f904
Anu Singh <anusingh1883656@gmail.com>
2020-12-13 13:04:20 +0530

    change in layout

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
-var x =0;
-var textEffect = "I'm Anushka and I love web Development!!"
-var container = document.getElementById('effect')
-var pic = document.getElementById('image1')
 
-function animate(){
-    if(x < textEffect.length){
-        container.innerHTML += textEffect.charAt(x)
-        x++;
-        setTimeout(animate, 100)
-    }
-}
-animate()
+const checkbox = document.getElementById('checkbox');
 
+checkbox.addEventListener('change', () => {
+    document.body.classList.toggle('dark');
+}); 
